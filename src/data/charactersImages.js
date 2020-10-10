5bfec78f697aa75cc57a2cbfd0d42bfacb8bf055
lovelaught <darya.bratova13@gmail.com>
2020-10-10 18:12:37 +0300

    fix images access

--- src/data/charactersImages.js
+++ src/data/charactersImages.js
@@ -1,8 +1,15 @@
+import Rachel from "../assets/characters/Rachel.png";
+import Ross from "../assets/characters/Ross.png";
+import Phoebe from "../assets/characters/Phoebe.png";
+import Monica from "../assets/characters/Monica.png";
+import Chandler from "../assets/characters/Chandler.png";
+import Joey from "../assets/characters/Joey.png";
+
 export const charactersImages = {
-  Rachel: "/characters/Rachel.png",
-  Ross: "/characters/Ross.png",
-  Phoebe: "/characters/Phoebe.png",
-  Monica: "/characters/Monica.png",
-  Chandler: "/characters/Chandler.png",
-  Joey: "/characters/Joey.png",
+  Rachel,
+  Ross,
+  Phoebe,
+  Monica,
+  Chandler,
+  Joey,
 };
